89e35a152ae8777e94cb7597256ab23ecda0b1d8
ksankaseam <ksankaseam@gmail.com>
2017-12-13 22:05:21 +0700

    Edit Check timebox
    Set timebox for 21:00-2:00

--- app/src/main/java/com/example/pacharapoldeesawat/demohospital/CheckTimeBox.java
+++ app/src/main/java/com/example/pacharapoldeesawat/demohospital/CheckTimeBox.java
@@ -1,30 +1,51 @@
 package com.example.pacharapoldeesawat.demohospital;
 
+
 import android.util.Log;
 
-/**
- * Created by pacharapoldeesawat on 11/1/2017 AD.
- */
+
 
 public class CheckTimeBox {
 
+
     private static int timebox;
+
 
     public static int checkTimeBox(int hour, int min){
 
+
+
         switch (hour) {
-            case 18: timebox = (min < 30) ? 1 : 2;
+
+            case 21: timebox = (min < 30) ? 1 : 2;
+
                 break;
-            case 19: timebox = (min < 30) ? 3 : 4;
+
+            case 22: timebox = (min < 30) ? 3 : 4;
+
                 break;
-            case 20: timebox = (min < 30) ? 5 : 6;
+
+            case 23: timebox = (min < 30) ? 5 : 6;
+
                 break;
-            case 21: timebox = (min < 30) ? 7 : 8;
+
+            case 0: timebox = (min < 30) ? 7 : 8;
+
                 break;
-            case 22: timebox = (min < 30) ? 9 : 10;
+
+            case 1: timebox = (min < 30) ? 9 : 10;
+
                 break;
+
+            default:   timebox = 0;
+
+                break;
+
         }
+
         return timebox;
 
+
     }
+
 }
